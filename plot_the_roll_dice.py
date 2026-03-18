5a65ffe5d396f2c5d37559ebd675e5ed0c982b13
dawitwoldtinsae-glitch <dawit.woldtinsae@aastustudent.edu.et>
2026-03-18 10:37:37 +0300

    Add files via upload

---
+++ plot_the_roll_dice.py
@@ -0,0 +1,17 @@
+import plotly.express as px
+from dices_class import Die
+
+die=Die()
+die_2=Die()
+results=[die.roll()*die_2.roll() for roll_num in range(1000)]
+
+max_result=die.num_sides*die_2.num_sides
+poss_results=range(1,max_result+1)
+frequencies=[results.count(value) for value in poss_results]
+
+title="Results of rolling three D6 dices 1000 Times"
+labels={"x":'result','y':'Frequency of result'}
+fig=px.bar(x=poss_results,y=frequencies,title=title,labels=labels)
+
+fig.update_layout(xaxis_dtick=1)
+fig.show()
